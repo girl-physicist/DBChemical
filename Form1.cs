00f63f8804e3b6ab5ee66229227ec95250f055d4
Anastasiya <girl-physicist@live.com>
2022-10-28 22:16:12 +0300

    delete and update

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,16 +15,16 @@ namespace DBChemical
         private DataSet _dataSet = null;
         private int _numerLastColumn;
         private bool _newRowAdd = false;
-        private const string ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\girl-\\source\\repos\\ProjectDataBase\\Database1.mdf;Integrated Security=True";
+        private const string _connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\girl-\\source\\repos\\ProjectDataBase\\Database1.mdf;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
             Element = new ChemicalElementModal();
-            _connection = new SqlConnection(ConnectionString);
+            _connection = new SqlConnection(_connectionString);
         }
         private async void Form1_Load(object sender, EventArgs e)
         {
-            await ConnectWithDb(GetData);
+            await ConnectWithDB(GetData);
         }
         private void ReloadData()
         {
@@ -31,20 +32,25 @@ namespace DBChemical
             {
                 _dataSet.Tables["Elements"].Clear();
                 _adapter.Fill(_dataSet, "Elements");
+                ShowData();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Ошибка!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            ShowData();
+
 
         }
         private SqlDataAdapter CreateAdapter(string sqlCommand, SqlConnection connection)
         {
             var command = new SqlCommand(sqlCommand, connection);
-            return new SqlDataAdapter(command);
+            var adapter = new SqlDataAdapter(command);
+            var _sqlBuilder = new SqlCommandBuilder(adapter);
+            _sqlBuilder.GetDeleteCommand();
+            _sqlBuilder.GetUpdateCommand();
+            return adapter;
         }
-        public async Task ConnectWithDb(Action<SqlConnection> action)
+        public async Task ConnectWithDB(Action<SqlConnection> action)
         {
             try
             {
@@ -69,7 +75,7 @@ namespace DBChemical
         }
         private void GetData(SqlConnection connection)
         {
-            _adapter = new SqlDataAdapter("SELECT *, 'Delete' AS [Delete] FROM [Elements]", connection);
+            _adapter = CreateAdapter("SELECT *, 'Delete' AS [Delete] FROM [Elements]", connection);
         }
         private void ShowData()
         {
@@ -77,9 +83,9 @@ namespace DBChemical
             {
                 dataGridView1.DataSource = _dataSet.Tables["Elements"];
                 _numerLastColumn = dataGridView1.ColumnCount - 1;
-                for (var i = 0; i < dataGridView1.Rows.Count; i++)
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
                 {
-                    var linkCell = new DataGridViewLinkCell();
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
                     dataGridView1[_numerLastColumn, i] = linkCell;
                 }
             }
@@ -90,7 +96,7 @@ namespace DBChemical
         }
         private void FindElementByName(SqlConnection connection)
         {
-            _adapter = CreateAdapter($"SELECT *, 'Delete' AS [Delete] FROM [Elements] where [nameElement]  = '{textBoxName.Text}'", connection);
+            _adapter = CreateAdapter($"SELECT *, 'Delete' AS [Delete] FROM [Elements] where [nameElement] = '{textBoxName.Text}'", connection);
         }
         public void Insert(SqlConnection connection)
         {
@@ -101,55 +107,22 @@ namespace DBChemical
         }
         public async Task Insert()
         {
-            await ConnectWithDb(Insert);
-            await ConnectWithDb(GetData);
+            await ConnectWithDB(Insert);
+            await ConnectWithDB(GetData);
         }
-        private void DataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        private async void ButtonShowDB_Click(object sender, EventArgs e)
         {
-            var rowIndex = 0;
-                try
-                {
-                    if (e.ColumnIndex == _numerLastColumn)
-                    {
-                        string task = dataGridView1.Rows[e.RowIndex].Cells[_numerLastColumn].Value.ToString();
-                        switch (task)
-                        {
-                            case "Delete":
-                            {
-                                if (MessageBox.Show("Вы действительно хотите удалить эту строку?", "Удаление строки",
-                                        MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-                                    == DialogResult.Yes)
-                                {
-                                    rowIndex = e.RowIndex;
-                                    dataGridView1.Rows.RemoveAt(rowIndex);
-                                    _adapter.Update(_dataSet, "Elements");
-                                }
 
-                                break;
-                            }
-                            case "Update":
-                            {
-                                var r = e.RowIndex;
-                                _dataSet.Tables["Elements"].Rows[r][1] = dataGridView1.Rows[r].Cells[1].Value;
-                                _dataSet.Tables["Elements"].Rows[r][2] = dataGridView1.Rows[r].Cells[2].Value;
-                                _dataSet.Tables["Elements"].Rows[r][3] = dataGridView1.Rows[r].Cells[3].Value;
-                                _dataSet.Tables["Elements"].Rows[r][4] = dataGridView1.Rows[r].Cells[4].Value;
-                                _dataSet.Tables["Elements"].Rows[r][5] = dataGridView1.Rows[r].Cells[5].Value;
-                                _dataSet.Tables["Elements"].Rows[r][6] = dataGridView1.Rows[r].Cells[6].Value;
-                                _adapter.Update(_dataSet, "Elements");
-                                dataGridView1.Rows[e.RowIndex].Cells[_numerLastColumn].Value = "Delete";
-                                break;
-                            }
-                        }
-
-                        ReloadData();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Ошибка!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            
+            await ConnectWithDB(GetData);
+        }
+        private async void ButtonFind_Click(object sender, EventArgs e)
+        {
+            await ConnectWithDB(FindElementByName);
+        }
+        private void ButtonInsert_Click(object sender, EventArgs e)
+        {
+            FormInsertcs form = new FormInsertcs(this);
+            form.Show();
         }
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
@@ -169,21 +142,58 @@ namespace DBChemical
                 MessageBox.Show(ex.Message, "Ошибка!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        private async void ButtonShowDB_Click(object sender, EventArgs e)
-        {
 
-            await ConnectWithDb(GetData);
-        }
-        private async void ButtonFind_Click(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            await ConnectWithDb(FindElementByName);
-        }
-        private void ButtonInsert_Click(object sender, EventArgs e)
-        {
-            FormInsertcs form = new FormInsertcs(this);
-            form.Show();
-        }
+            var rowIndex = 0;
+            var adapter = CreateAdapter("SELECT *, 'Delete' AS [Delete] FROM [Elements]", _connection);
+            try
+            {
+                if (e.ColumnIndex == _numerLastColumn)
+                {
+                    string task = dataGridView1.Rows[e.RowIndex].Cells[_numerLastColumn].Value.ToString();
+                    if (task == "Delete")
+                    {
+                        if (MessageBox.Show("Вы действительно хотите удалить эту строку?", "Удаление строки",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                            == DialogResult.Yes)
+                        {
+                            rowIndex = e.RowIndex;
+                            dataGridView1.Rows.RemoveAt(rowIndex);
 
+                            adapter.Update(_dataSet, "Elements");
+
+
+
+                        }
+                    }
+                    else if (task == "Update")
+                    {
+
+                        int r = e.RowIndex;
+
+                        _dataSet.Tables["Elements"].Rows[r][1] = dataGridView1.Rows[r].Cells[1].Value;
+                        _dataSet.Tables["Elements"].Rows[r][2] = dataGridView1.Rows[r].Cells[2].Value;
+                        _dataSet.Tables["Elements"].Rows[r][3] = dataGridView1.Rows[r].Cells[3].Value;
+                        _dataSet.Tables["Elements"].Rows[r][4] = dataGridView1.Rows[r].Cells[4].Value;
+                        _dataSet.Tables["Elements"].Rows[r][5] = dataGridView1.Rows[r].Cells[5].Value;
+                        _dataSet.Tables["Elements"].Rows[r][6] = dataGridView1.Rows[r].Cells[6].Value;
+
+                        adapter.Update(_dataSet, "Elements");
+
+                        dataGridView1.Rows[e.RowIndex].Cells[_numerLastColumn].Value = "Delete";
+                    }
+                    ReloadData();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
     }
 }
+
+
 
